094ea76670bbef6a549b59ec9de0612a95405929
MahakGupta03 <98971021+MahakGupta03@users.noreply.github.com>
2023-05-08 17:54:19 +0530

    Add files via upload

---
+++ New folder/index.js
@@ -0,0 +1,95 @@
+console.log("Hello world");
+// alert("hi");
+// document.write("This is document")
+// console.warn("This is a warning")
+// console.error("This is an error")
+
+// var a = 5;
+// var b = 5;
+// console.log(a+b);
+
+// var o = {
+//     age : 10,
+//     name : "Mahak",
+//     rollno : 5
+// };
+// console.log(o);
+
+function avg(a,b) {
+    return (a+b)/2;
+}
+
+var c1 = avg(5,5);
+var c2 = avg(9,6);
+// console.log(c1);
+// console.log(c2);
+
+
+let mydate = new Date();
+// console.log(mydate);
+// console.log(mydate.getDay());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getHours());
+// console.log(mydate.getMonth());
+// console.log(mydate.getTime());
+
+
+// document.getElementById("click").style.border = "2px solid blue";
+// console.log(document.getElementById("click").innerHTML);
+
+// let eleClass = document.getElementsByClassName("container")
+// console.log(eleClass);
+// eleClass[0].classList.add("bg-primary");
+
+// console.log(eleClass[0].innerHTML);
+// console.log(eleClass[0].innerText)
+
+// tn = document.getElementsByTagName("div");
+// console.log(tn)
+// createElement = document.createElement("p");
+// createElement.innerHTML = "This is a created element";
+// tn[0].appendChild(createElement);
+
+
+// function clicked() {
+//     console.log("clicked");
+// }
+
+// secondContainer.addEventListener("click",function () {
+//     console.log("click on container")
+// })
+
+// firstContainer.addEventListener("mouseover",function () {
+//     console.log("Moving on container")
+// })
+// firstContainer.addEventListener("mouseout",function () {
+//     console.log("Moving out of container")
+// })
+
+// let prevHTML = document.querySelectorAll(".container")[1].innerHTML;
+// secondContainer.addEventListener("mouseup",function () {
+//         document.querySelectorAll(".container")[1].innerHTML = prevHTML;
+//         // console.log("Moving on container")
+// })
+
+// secondContainer.addEventListener("mousedown",function () {
+//     document.querySelectorAll(".container")[1].innerHTML = "The changed text";
+//     // console.log("Moving on container")
+// })
+
+
+// logkaro = () =>{
+//     document.querySelectorAll(".container")[1].innerHTML = "The changed text";
+//     console.log("Loging");
+// }
+// setTimeout(logkaro,2000);
+// setInterval(logkaro, 2000);
+
+
+// let obj = {name : "yash", length: 1, a: {this : "that"}}
+// jso = JSON.stringify(obj);
+// console.log(jso)
+
+// parsed = JSON.parse(jso);
+// console.log(parsed);
+
